9eec35af26666bbe5420d7ee0f3c2df200bc7c00
Max Omdal <mjomdal@gmail.com>
2023-11-05 20:57:39 +0000

    Some filler content for website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import dizzy from './dizzy.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={dizzy} alt="dog"/>
+        <h1>Sequoia Fabrica</h1>
+        <h2>A San Francisco based workshop</h2>
       </header>
+      <p>Lorem Ipsum dolor set amet </p>
     </div>
   );
 }
