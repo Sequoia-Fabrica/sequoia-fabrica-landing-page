a1bf6a994883f7662e687fe602e26892eae26330
Maxwell Omdal <omdal009@umn.edu>
2023-11-05 22:27:25 -0800

    Add some filler content

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Sequoia Fabrica</h1>
-        <h2>A San Francisco based workshop</h2>
+        <h2>A San Francisco Community Workshop</h2>
       </header>
-      <p>Lorem Ipsum dolor set amet </p>
+
+      <div className="About">
+      <h2>Upcoming Events</h2>
+
+      <h2>Our Mission</h2>
+      <p>We aim to provide a space to disseminate knowledge and craftsmanship skills to a diverse community of people working in multiple disciplines of interest</p>
+      <p>Sequoia Fabrica (or SeqFab) grew out of other San Francisco makerspaces and hackerspaces with the intention of creating a space focused on building tangible projects through wood, textiles, and electronics. We expect the space to continually undergo change as it ages and welcomes new community members</p>
+
+      <h2>Membership Opportunities</h2>
+      <p>We will be open to the general public for regular, hosted events and workshops. If you’d like access to the space or use our equipment at other times, you should become a member!</p>
+      <p>We’re aiming for standard monthly dues similar to other Bay Area workshops, about $100 a month. We are committed to serving our community and want to ensure no one is turned away from tools and knowledge due to lack of funds.</p>
+
+      <h2>Donations</h2>
+      <p>We are a California Public Benefit Corporation and have filed an application for 501(c)(3) status with the IRS. Our application is pending. If this is approved your <a href="https://www.irs.gov/charities-non-profits/charitable-organizations/contributions-to-organization-with-irs-application-pending">contribution may be tax-deductible</a>.</p>
+
+      <h2>Location</h2>
+      <p>We are in the process of securing a space for our community. The space will be in San Francisco</p>
+      </div>
     </div>
   );
 }
