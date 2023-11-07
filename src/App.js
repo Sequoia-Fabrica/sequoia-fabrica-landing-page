e2a78a000b6c096cba98f62bdb5d1b81bd7b1b6c
Maxwell Omdal <omdal009@umn.edu>
2023-11-06 22:31:51 -0800

    Add tinyletter link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import DOMPurify from "dompurify";
 import './App.css';
 
 function App() {
+
+    const tinyLetterHTML = `<form style="border:1px solid #ccc;padding:3px;text-align:center;" action="https://tinyletter.com/sequoiafabrica" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/sequoiafabrica', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"><p><label for="tlemail">Enter your email address</label></p><p><input type="text" style="width:140px" name="email" id="tlemail" /></p><input type="hidden" value="1" name="embed"/><input type="submit" value="Subscribe" /></form>`;
+  const tinyLetterSanitized = DOMPurify.sanitize(tinyLetterHTML);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +19,10 @@ function App() {
       <h2>Our Mission</h2>
       <p>We aim to provide a space to disseminate knowledge and craftsmanship skills to a diverse community of people working in multiple disciplines of interest</p>
       <p>Sequoia Fabrica (or SeqFab) grew out of other San Francisco makerspaces and hackerspaces with the intention of creating a space focused on building tangible projects through wood, textiles, and electronics. We expect the space to continually undergo change as it ages and welcomes new community members</p>
+
+      <h2>Mailing List</h2>
+      Want to learn more about our space and hear updates on our search for a physical home? Join our mailing list!
+      <div dangerouslySetInnerHTML={{ __html: tinyLetterSanitized }} />
 
       <h2>Membership Opportunities</h2>
       <p>We will be open to the general public for regular, hosted events and workshops. If you’d like access to the space or use our equipment at other times, you should become a member!</p>
