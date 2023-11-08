a6b81dbd111ac8b80e7005dbbc960d5fb43a67af
Maxwell Omdal <omdal009@umn.edu>
2023-11-07 21:20:31 -0800

    clean up UI; Add description to upcoming events

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
 
-    const tinyLetterHTML = `<form style="border:1px solid #ccc;padding:3px;text-align:center;" action="https://tinyletter.com/sequoiafabrica" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/sequoiafabrica', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"><p><label for="tlemail">Enter your email address</label></p><p><input type="text" style="width:140px" name="email" id="tlemail" /></p><input type="hidden" value="1" name="embed"/><input type="submit" value="Subscribe" /></form>`;
+    const tinyLetterHTML = `<form style="border:1px; margin-left:20%; margin-right:20%; solid #ccc;padding:3px;text-align:center;" action="https://tinyletter.com/sequoiafabrica" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/sequoiafabrica', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"><p><label for="tlemail">Enter your email address</label></p><p><input type="text" style="width:140px" name="email" id="tlemail" /></p><input type="hidden" value="1" name="embed"/><input type="submit" value="Subscribe" /></form>`;
   const tinyLetterSanitized = DOMPurify.sanitize(tinyLetterHTML);
 
   return (
@@ -15,6 +15,7 @@ function App() {
 
       <div className="About">
       <h2>Upcoming Events</h2>
+      <p>Stay tuned for events as we get going!</p>
 
       <h2>Our Mission</h2>
       <p>We aim to provide a space to disseminate knowledge and craftsmanship skills to a diverse community of people working in multiple disciplines of interest</p>
