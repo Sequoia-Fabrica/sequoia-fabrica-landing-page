90e135e88c079a451ab9d3122f74115b49099cfb
momja <omdal009@umn.edu>
2023-12-03 18:33:55 +0000

    Deploying to gh-pages from @ momja/sequoia-fabrica-landing-page@3448ca034a4c6a98b307c0fe90804abfbabeb306 🚀

---
+++ _next/static/1YP9gcLKQLwEHMHFwYmhY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/109-03439cea6ea22caf.js","static/chunks/pages/index-26f8bfe5f0dd0556.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
