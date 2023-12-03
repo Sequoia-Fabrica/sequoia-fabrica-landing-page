23f8674558e49721e6e05c5c8b9d023d68438f0b
Max Omdal <maxomdal@Emmas-MacBook-Air.local>
2023-12-03 10:31:53 -0800

    Setup nextjs for static site building

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'build',
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
