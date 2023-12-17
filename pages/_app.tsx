13568d92f3d2ba12a3e986f1a14e813764425951
Maxwell Omdal <maxwell.omdal@veeva.com>
2023-12-16 19:26:17 -0600

    add analytics to website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script data-goatcounter="https://sequoiafabrica.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
