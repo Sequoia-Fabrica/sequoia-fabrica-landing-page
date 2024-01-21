a1ca90ae1ad8b0c43b6b25ba8d1a3e4f3b40bc8a
Max Omdal <mjomdal@gmail.com>
2024-01-20 23:20:47 -0800

    fix incorrect mailchimp URL

--- src/text/urlConstants.ts
+++ src/text/urlConstants.ts
@@ -3,5 +3,5 @@ export const urlConstants = {
     instagram: "https://www.instagram.com/sequoia.fabrica/",
     meetup: "https://www.meetup.com/sequoia-fabrica-community-workshop-and-classes/",
     openCollective: "https://opencollective.com/sequoia-fabrica",
-    mailchimp: "https://sequoiafabrica.us21.list-manage.com/subscribe/post?u=e67b644f9532ceb232cf38ac2&amp;id=28291a8406&amp;f_id=00e7f6e6f0",
+    mailchimp: "http://eepurl.com/iIprpk",
 }
