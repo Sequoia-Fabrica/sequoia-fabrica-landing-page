6d0b2a6359374a1237b4f4ed0c31aafaa9f9f847
e-mln-e <brule.emeline@gmail.com>
2024-01-23 13:52:58 -0800

    Update DocumentLayout.tsx
    add vertical spacing

--- src/components/docs/DocumentLayout.tsx
+++ src/components/docs/DocumentLayout.tsx
@@ -2,7 +2,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
     // Create any shared layout or styles here
     return <div>
         <div>
-            <div className="m-auto max-w-2xl p-5">{children}</div>
+            <div className="m-auto max-w-2xl p-5 my-8">{children}</div>
         </div>
     </div>
 }
