56784dd677ee15d1102fdaeee8305727356d26ef
e-mln-e <brule.emeline@gmail.com>
2024-01-23 13:55:03 -0800

    Update MenuBar.tsx

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -13,7 +13,7 @@ const MenuBar: React.FC<MenuBarProps> = ({ isMenuOpen = false }) => {
     
 
     return (
-      <ul className="p-4 bg-tea_green-800 absolute list-none w-full rounded-b-lg">
+      <ul className="p-4 bg-emerald-900 absolute list-none w-full rounded-b-lg text-2xl">
           <MenuBarItem name='Events' url={urlConstants.meetup} />
           <MenuBarItem name='Donate' url={urlConstants.openCollective} />
           <MenuBarItem name='Membership' url='/docs/membership' />
