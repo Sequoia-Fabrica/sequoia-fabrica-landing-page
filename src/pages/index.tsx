1b81bfb124512c0526e97813c2bd90457e411719
e-mln-e <brule.emeline@gmail.com>
2024-01-25 20:04:37 +0000

    responsive design homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
   return (
     <div className="App">
-      <div className="About m-4 max-w-screen-md mx-auto grid grid-cols-2 gap-4">
+      <div className="About m-4 max-w-screen-md mx-auto grid grid-cols-2 gap-4 p-8">
         <div className="col-span-2">
             <p>We are Sequoia Fabrica, an intergenerational community workshop in the <a href="https://maps.app.goo.gl/pfgdThhr7yy42nGZA">San Francisco’s Potrero Hill neighborhood</a> fostering the next generation of makers, designers and craftspeople. We’re home to a wood and textile workshop, 3D printers and laser cutter, electronics, crafts and fine arts stations. </p>
             <p>We are a volunteer-run non-profit (501c3 status pending), and host <a href="https://www.meetup.com/sequoia-fabrica-community-workshop-and-classes/">classes, events and workshop hours</a> open to the public. </p>
@@ -41,22 +41,22 @@ export default function Home() {
             <hr/>
           </div>
         
-        <div className="col-span-1">
+        <div className="col-span-2 md:col-span-1">
           <h2>Visit and Learn a New Skill</h2>
           <p>We will be opening to the public gradually from February 17. Sign-up <a href="https://www.meetup.com/sequoia-fabrica-community-workshop-and-classes/">for a visit or a class</a>.</p>
         </div>  
-        <div className="col-span-1">
+        <div className="col-span-2 md:col-span-1">
           <h2 >Stay in Touch!</h2>
-          <div className=""></div><div dangerouslySetInnerHTML={{ __html: mailchimpSanitized }} />
-          <p><a href="https://instagram.com/sequoia.fabrica">Instagram</a>, <a href="https://sfba.social/@sequoiafabrica">Mastodon</a></p>
+          <div className="float-left col-span-2"></div><div dangerouslySetInnerHTML={{ __html: mailchimpSanitized }} />
+          <p className=" col-span-2 clear-both"><a href="https://instagram.com/sequoia.fabrica" >Instagram</a>, <a href="https://sfba.social/@sequoiafabrica">Mastodon</a></p>
           <p><a href="/docs/membership">Become a member</a></p>
         </div>  
         <hr className="col-span-2"/>
-        <div className="col-span-1">
+        <div className="col-span-2 md:col-span-1">
           <h2>Donations</h2>
           <p>Please support makers of today and tomorrow with a <a href="http://opencollective.com/sequoia-fabrica">donation</a>! We are a California Public Benefit Corporation and have filed an application for 501(c)(3) status with the IRS. Our application is pending.</p>
         </div>
-        <div className="col-span-1">
+        <div className="col-span-2 md:col-span-1">
           <h2>Code of Conduct</h2>
           <p>We have a <a href="/docs/code_of_conduct">Code of Conduct</a> that is required reading for all members. Anyone involved either in classes or otherwise is expected to review this.</p>
         </div>
