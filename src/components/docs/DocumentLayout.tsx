f116c2d18b3f33bf110af6e9880168ea6e233103
aligatorcat <155796318+aligatorcat@users.noreply.github.com>
2024-07-11 16:42:09 +0000

    Adding title and meta description

--- src/components/docs/DocumentLayout.tsx
+++ src/components/docs/DocumentLayout.tsx
@@ -1,10 +1,31 @@
-export default function MdxLayout({ children }: { children: React.ReactNode }) {
+import Head from "next/head"
+
+
+interface LayoutProps {
+  children: React.ReactNode;
+  metadata: { title?: string ; description?: string; };
+}
+
+
+
+export default function MdxLayout({ children, ...props}: LayoutProps ) {
     // Create any shared layout or styles here
-    return <div>
-        <div>
-            <div className="m-auto max-w-2xl p-5 my-8">
-                <div className="my-8">{children}</div>
+
+    const { title, description } = props.metadata ? props.metadata: {};
+    const defaultTitle = title ? `${title} | Sequoia Fabrica` : "Sequoia Fabrica";
+    const defaultDescription = description || "Default description";
+
+    // Create any shared layout or styles here
+    return (    
+    <div>
+            <Head>
+                    <title key="metatitle">{defaultTitle}</title>
+                    <meta key="metadesc" name="description" content={defaultDescription} />
+            </Head>
+            <div>
+                    <div className="m-auto max-w-2xl p-5 my-8">
+                            <div className="my-8">{children}</div>
+                    </div>
             </div>
-        </div>
-    </div>
+    </div>)
 }
