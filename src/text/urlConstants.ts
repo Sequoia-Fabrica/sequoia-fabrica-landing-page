ff9f68778ecef2ad07165ac4f92b32d16e6cb3cf
Max Omdal <mjomdal@gmail.com>
2025-11-10 08:32:08 -0800

    Update membership dues strategy

--- src/text/urlConstants.ts
+++ src/text/urlConstants.ts
@@ -1,13 +1,15 @@
 export const urlConstants = {
-  mailto: "mailto:sequoiafabrica@gmail.com",
-  instagram: "https://www.instagram.com/sequoia.fabrica",
-  meetup: "https://www.meetup.com/sequoia-fabrica-community-workshop-and-classes",
-  bookWhen: "https://bookwhen.com/sequoiafabrica",
-  donateOC: "https://opencollective.com/sequoia-fabrica",
-  mailchimp: "https://dashboard.mailerlite.com/forms/1197290/139047052633965606/share",
-  googleMaps: "https://maps.app.goo.gl/pfgdThhr7yy42nGZA",
-  mastodon: "https://sfba.social/@sequoiafabrica",
-  wiki: "https://wiki.sequoiafabrica.org/wiki",
-  tiktok: "https://www.tiktok.com/@sequoiafabrica",
-  donateStripe: "https://donate.stripe.com/eVa14Mef87bM7XGeUU"
+    email: "info@sequoiafabrica.org",
+    mailto: "mailto:info@sequoiafabrica.org",
+    instagram: "https://www.instagram.com/sequoia.fabrica",
+    meetup: "https://www.meetup.com/sequoia-fabrica-community-workshop-and-classes",
+    bookWhen: "https://bookwhen.com/sequoiafabrica",
+    bookWhenMembership: "https://bookwhen.com/sequoiafabricamembers/memberships",
+    donateOC: "https://opencollective.com/sequoia-fabrica",
+    mailchimp: "https://dashboard.mailerlite.com/forms/1197290/139047052633965606/share",
+    googleMaps: "https://maps.app.goo.gl/pfgdThhr7yy42nGZA",
+    mastodon: "https://sfba.social/@sequoiafabrica",
+    wiki: "https://wiki.sequoiafabrica.org/wiki",
+    tiktok: "https://www.tiktok.com/@sequoiafabrica",
+    donateStripe: "https://donate.stripe.com/eVa14Mef87bM7XGeUU"
 }
